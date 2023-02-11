4f4f9c124df79ecec50d2f8fba39f37833705de4
kurumadaisuke <kuruma0909@gmail.com>
2023-02-11 16:30:31 +0900

    コードレビュー後の修正 ・date(変数名)の変更 ・everyday(変数名)の変更 ・使わない変数を編集

--- 02.calendar/cal.rb
+++ 02.calendar/cal.rb
@@ -1,49 +1,37 @@
 require 'date'
 require 'optparse'
 
-today_year = Date.today.year
-today_month = Date.today.month
-
 opt = OptionParser.new
 option = {}
 opt.on('-y [value]'){|v| option[:y] = v }
 opt.on('-m [value]'){|v| option[:m] = v }
 opt.parse(ARGV)
 
-year = option[:y].to_i
-month = option[:m].to_i
+year = option[:y].to_i == 0 ? Date.today.year : option[:y].to_i
+month = option[:m].to_i == 0 ? Date.today.month : option[:m].to_i
 
-if year == 0 
-  year = today_year
-end
-
-if month == 0 
-  month = today_month
-end
-
-
-date = Date.new(year, month, -1)
-puts date.strftime("%m月 %Y").center(18)
+end_of_month = Date.new(year, month, -1)
+puts end_of_month.strftime("%m月 %Y").center(18)
 puts "日 月 火 水 木 金 土"
 
-days = (1..date.day)
+days = (1..end_of_month.day)
 days.each do |day|
-  everyday = Date.new(year, month, day)
+  each_day = Date.new(year, month, day)
 
-  if everyday.day == 1
-    case everyday.cwday
+  if each_day.day == 1
+    case each_day.cwday
     when 7
       print ""
     else
-      week = (everyday.cwday)*3
+      week = (each_day.cwday)*3
       print "".to_s.ljust(week)
     end
   end
 
-  if everyday.saturday?
-    print everyday.day.to_s.ljust(3)
+  if each_day.saturday?
+    print each_day.day.to_s.ljust(3)
     print "\n"
   else
-    print everyday.day.to_s.ljust(3) 
+    print each_day.day.to_s.ljust(3) 
   end
 end
