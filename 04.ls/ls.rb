6411f20ccd9d0b47804584fb3a5f62df45a20ce6
kurumadaisuke <kuruma0909@gmail.com>
2023-02-26 22:02:51 +0900

    ほとんど完成。rubocopをする。

--- 04.ls/ls.rb
+++ 04.ls/ls.rb
@@ -1,26 +1,27 @@
 #!/usr/bin/env ruby
 require 'debug'
 
-filenames = Dir.glob("*") # ファイル名一覧の取得
-frames = []
-
-filenames.each do |filename| # idとファイル名をハッシュで管理
-  frames << filename
+def filename_acquisition(filenames) # ファイル名一覧の取得
+  @frames = filenames
+  filename_output
 end
 
-column_length = frames.each_slice(3).to_a.length # 4列の場合[3]のところを4にする
-column_array = frames.each_slice(column_length).to_a
+def filename_output
+  column_length = @frames.each_slice(3).to_a.length # 4列の場合[3]のところを4にする
+  column_array = @frames.each_slice(column_length).to_a
 
-if column_array[-1].size < column_length
-  loop{
-    column_array[-1] << nil
-    break if column_array[-1].size >= column_length
-  }
+  if column_array[-1].size < column_length
+    loop{
+      column_array[-1] << nil
+      break if column_array[-1].size >= column_length
+    }
+  end
+
+  column_array.transpose.each_with_index do |row, i| # ファイル名を順番に合わせて出力する
+    row.map{|h| print h.to_s.ljust(16)}
+    print "\n" if !(i == column_array.transpose.length - 1)
+  end
 end
 
-column_array.transpose.each_with_index do |row, i| # ファイル名を順番に合わせて出力する
-  row.map{|h| print h.to_s.ljust(16)}
-  print "\n" if !(i == column_array.transpose.length - 1)
-end
-
+filename_acquisition(Dir.glob("*"))
 # binding.break
