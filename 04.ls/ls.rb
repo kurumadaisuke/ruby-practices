79489e2694214096e87865014624726932ce615e
kurumadaisuke <kuruma0909@gmail.com>
2023-02-27 22:12:41 +0900

    プラクティス完成したので提出

--- 04.ls/ls.rb
+++ 04.ls/ls.rb
@@ -1,8 +1,8 @@
 #!/usr/bin/env ruby
-require 'debug'
+# frozen_string_literal: true
 
-def filename_acquisition(filenames) # ファイル名一覧の取得
-  @frames = filenames
+def filename_acquisition
+  @frames = Dir.glob("*")
   filename_output
 end
 
@@ -10,18 +10,17 @@ def filename_output
   column_length = @frames.each_slice(3).to_a.length # 4列の場合[3]のところを4にする
   column_array = @frames.each_slice(column_length).to_a
 
-  if column_array[-1].size < column_length
-    loop{
+  if column_array[-1].size < column_length # 各配列の数を合わせるためnilを入れる
+    loop do
       column_array[-1] << nil
       break if column_array[-1].size >= column_length
-    }
+    end
   end
 
-  column_array.transpose.each_with_index do |row, i| # ファイル名を順番に合わせて出力する
-    row.map{|h| print h.to_s.ljust(16)}
-    print "\n" if !(i == column_array.transpose.length - 1)
+  column_array.transpose.each_with_index do |row, i| # ファイル名を出力する
+    row.map { |h| print h.to_s.ljust(16) }
+    print "\n" if i != column_array.transpose.length - 1 # 最後だけ改行をしない
   end
 end
 
-filename_acquisition(Dir.glob("*"))
-# binding.break
+filename_acquisition
