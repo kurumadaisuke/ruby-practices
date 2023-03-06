c288121de9278462f71bb8244f3d884a1200eba9
kurumadaisuke <kuruma0909@gmail.com>
2023-03-06 23:19:22 +0900

    修正点すべて修正しました。

--- 04.ls/ls.rb
+++ 04.ls/ls.rb
@@ -11,15 +11,16 @@ end
 def filename_output
   column_length = @frames.each_slice(NUMBER_OF_COLUMNS).to_a.length
   column_array = @frames.each_slice(column_length).to_a
-  
+
   if column_array[-1].size < column_length # 各配列の数を合わせるためnilを入れる
     substitution_nil = column_length - column_array[-1].size
     substitution_nil.times { column_array[-1] << nil }
   end
-  
-  column_array.transpose.each_with_index do |row, i| # ファイル名を出力する
+
+  transposed = column_array.transpose
+  transposed.each_with_index do |row, i|
     row.each { |h| print h.to_s.ljust(16) }
-    print "\n" if i != column_array.transpose.length - 1 # 最後だけ改行をしない
+    print "\n" if i != transposed.length - 1
   end
 end
 
