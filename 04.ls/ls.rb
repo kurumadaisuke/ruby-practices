1275c603e60f049eb5f8d4efda20ba45bf24882a
kurumadaisuke <kuruma0909@gmail.com>
2023-03-06 20:43:56 +0900

    コード修正

--- 04.ls/ls.rb
+++ 04.ls/ls.rb
@@ -1,24 +1,24 @@
 #!/usr/bin/env ruby
 # frozen_string_literal: true
 
+NUMBER_OF_COLUMNS = 3 # 列数の定義
+
 def filename_acquisition
-  @frames = Dir.glob("*")
+  @frames = Dir.glob('*')
   filename_output
 end
 
 def filename_output
-  column_length = @frames.each_slice(3).to_a.length # 4列の場合[3]のところを4にする
+  column_length = @frames.each_slice(NUMBER_OF_COLUMNS).to_a.length
   column_array = @frames.each_slice(column_length).to_a
 
   if column_array[-1].size < column_length # 各配列の数を合わせるためnilを入れる
-    loop do
-      column_array[-1] << nil
-      break if column_array[-1].size >= column_length
-    end
+    substitution_nil = column_length - column_array[-1].size
+    substitution_nil.times { column_array[-1] << nil }
   end
 
   column_array.transpose.each_with_index do |row, i| # ファイル名を出力する
-    row.map { |h| print h.to_s.ljust(16) }
+    row.each { |h| print h.to_s.ljust(16) }
     print "\n" if i != column_array.transpose.length - 1 # 最後だけ改行をしない
   end
 end
