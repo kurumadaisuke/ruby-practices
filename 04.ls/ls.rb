b8715e2ead6a3ef4a0f3a5d32dc3a0f1e7a71064
kurumadaisuke <kuruma0909@gmail.com>
2023-03-16 22:17:51 +0900

    リファクタリングのためのバックアップ用

--- 04.ls/ls.rb
+++ 04.ls/ls.rb
@@ -2,6 +2,9 @@
 # frozen_string_literal: true
 
 require 'optparse'
+require 'tempfile'
+require 'etc'
+require 'time'
 
 NUMBER_OF_COLUMNS = 3 # 列数の定義（数字を変える）
 
@@ -13,6 +16,113 @@ end
 def reverse_filename_acquisition
   @frames = Dir.glob('*').reverse
   filename_output
+end
+
+def long_filename_acquisition
+  frames = Dir.glob('*')
+
+  total_block_size = []
+  frames.each do |filename|
+    y = File.stat(filename)
+    total_block_size <<  y.blocks
+  end
+  puts "total #{total_block_size.sum}"
+
+  frames.each do |filename|
+    y = File.stat(filename)
+    space = "  "
+    filetype = 
+      case y.ftype
+        when "fifo"
+          "p"
+        when "characterSpecial"
+          "c"
+        when "directory"
+          "d"
+        when "blockSpecial"
+          "b"
+        when "file"
+          "-"
+        when "link"
+          "l"
+        when "socket"
+          "s"
+      end
+      
+    permission = "%06d"% y.mode.to_s(8)
+    owner_permission =
+      case permission.slice(3, 1)
+        when "0"
+          "---"
+        when "1"
+          "--x"
+        when "2"
+          "-w-"
+        when "3"
+          "-wx"
+        when "4"
+          "r--"
+        when "5"
+          "r-x"
+        when "6"
+          "rw-"
+        when "7"
+          "rwx"
+        end
+
+    owner_group_permission =
+      case permission.slice(4, 1)
+        when "0"
+          "---"
+        when "1"
+          "--x"
+        when "2"
+          "-w-"
+        when "3"
+          "-wx"
+        when "4"
+          "r--"
+        when "5"
+          "r-x"
+        when "6"
+          "rw-"
+        when "7"
+          "rwx"
+        end
+    
+    other_permission =
+      case permission.slice(5, 1)
+        when "0"
+          "---"
+        when "1"
+          "--x"
+        when "2"
+          "-w-"
+        when "3"
+          "-wx"
+        when "4"
+          "r--"
+        when "5"
+          "r-x"
+        when "6"
+          "rw-"
+        when "7"
+          "rwx"
+        end
+    hard_link = y.nlink.to_s + space
+    user_name = Etc.getpwuid(y.uid).name + space
+    group_name = Etc.getgrgid(y.gid).name + space
+    file_size = y.size.to_s.rjust(4) + space
+    data_of_last_change = y.mtime.strftime("%m %d %H:%M") + space
+
+    print filetype + owner_permission + owner_group_permission + other_permission + space
+    puts hard_link +
+    user_name +
+    group_name +
+    file_size +
+    data_of_last_change +
+    filename
+  end
 end
 
 def default_filename_acquisition
@@ -39,6 +149,7 @@ end
 OptionParser.new do |opt|
   opt.on('-a', 'Show all file names.') { |o| @all = o }
   opt.on('-r', 'Reverse file order.') { |o| @reverse = o }
+  opt.on('-l', 'Long format file.') { |o| @long = o }
   begin
     opt.parse! # オプション解析してくれる
   rescue OptionParser::InvalidOption => e # 存在しないオプションを指定された場合
@@ -52,6 +163,8 @@ if @all == true
   all_filename_acquisition
 elsif @reverse == true
   reverse_filename_acquisition
+elsif @long == true
+  long_filename_acquisition
 else
   default_filename_acquisition
 end
