16f87c5461929bb76f4b85ce26bf20f36994075b
kurumadaisuke <kuruma0909@gmail.com>
2023-03-31 23:27:10 +0900

    rubocop対応

--- 05.wc/wc.rb
+++ 05.wc/wc.rb
@@ -1,50 +1,50 @@
 #!/usr/bin/env ruby
+# frozen_string_literal: true
 
 require 'optparse'
-require 'debug'
 
 @output = []
 @total = []
 
 def lines_option(filenames)
   @lines = []
-  filenames.each {|filename| @lines << File.read(filename).count("\n").to_s.rjust(8)}
+  filenames.each { |filename| @lines << File.read(filename).count("\n").to_s.rjust(8) }
   @output << @lines
-  if @acquisition_filenames.size >= 2
-    @total << filenames.sum { |file| File.read(file).count("\n") }.to_s.rjust(8)
-  end
+  return unless @acquisition_filenames.size >= 2
+
+  @total << filenames.sum { |file| File.read(file).count("\n") }.to_s.rjust(8)
 end
 
 def words_option(filenames)
   @words = []
-  filenames.each {|file| @words << File.read(file).split(/\s+/).size.to_s.rjust(7) }
+  filenames.each { |file| @words << File.read(file).split(/\s+/).size.to_s.rjust(7) }
   @output << @words
-  if @acquisition_filenames.size >= 2
-    @total << filenames.sum { |file| File.read(file).split.size }.to_s.rjust(7)
-  end
+  return unless @acquisition_filenames.size >= 2
+
+  @total << filenames.sum { |file| File.read(file).split.size }.to_s.rjust(7)
 end
 
 def bytes_option(filenames)
   @bytes = []
-  filenames.each {|file| @bytes << File.size(file).to_s.rjust(8) }
+  filenames.each { |file| @bytes << File.size(file).to_s.rjust(8) }
   @output << @bytes
-  if @acquisition_filenames.size >= 2
-    @total << filenames.sum { |file| File.size(file) }.to_s.rjust(8)
-  end
+  return unless @acquisition_filenames.size >= 2
+
+  @total << filenames.sum { |file| File.size(file) }.to_s.rjust(8)
 end
 
 def file_name(filenames)
   @filename = []
-  filenames.each {|file| @filename << file }
+  filenames.each { |file| @filename << file }
   @output << @filename
 end
 
 def output
   transposed = @output.transpose
   transposed.each { |row| puts row.join(' ') }
-  if @acquisition_filenames.size >= 2
-    puts "#{@total.join(' ')} total"
-  end
+  return unless @acquisition_filenames.size >= 2
+
+  puts "#{@total.join(' ')} total"
 end
 
 def pipe_output(output)
@@ -62,13 +62,13 @@ end
 
 begin
   option = ARGV.getopts('lwc')
-  if option['l']  == false && option['w'] == false && option['c'] == false && !ARGV.empty?
-    option['l'] = true && option['w'] = true && option['c'] = true
-  end
-    option['l'] ? lines_option(@acquisition_filenames) : nil
-    option['w'] ? words_option(@acquisition_filenames) : nil
-    option['c'] ? bytes_option(@acquisition_filenames) : nil
-  if !ARGV.empty?
+  option['l'] = true && option['w'] = true && option['c'] = true if
+  option['l'] == false && option['w'] == false && option['c'] == false && !ARGV.empty?
+
+  option['l'] ? lines_option(@acquisition_filenames) : nil
+  option['w'] ? words_option(@acquisition_filenames) : nil
+  option['c'] ? bytes_option(@acquisition_filenames) : nil
+  unless ARGV.empty?
     file_name(@acquisition_filenames)
     output
   end
