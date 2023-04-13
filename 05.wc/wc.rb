f18f729281453051073637b0a63ceb49c1b8c6f2
kurumadaisuke <kuruma0909@gmail.com>
2023-04-13 09:08:43 +0900

    すべて修正完了

--- 05.wc/wc.rb
+++ 05.wc/wc.rb
@@ -2,84 +2,74 @@
 # frozen_string_literal: true
 
 require 'optparse'
-require 'debug'
 
 def format(count)
   count.to_s.rjust(8)
 end
 
 def file_read(filenames)
-  file_content = []
-  filenames.each { |filename| file_content << File.read(filename) }
-  return file_content
+  file_contents = []
+  filenames.each { |filename| file_contents << File.read(filename) }
+  file_contents
 end
 
-def lines_count(filenames)
+def lines_count(file_contents)
   lines = []
-  filenames.each { |filename| lines << format(File.read(filename).count("\n")) }
-  return lines
+  file_contents.each { |content| lines << format(content.count("\n")) }
+  lines
 end
 
-def words_count(filenames)
+def words_count(file_contents)
   words = []
-  filenames.each { |file| words << format(File.read(file).split(/\s+/).size) }
-  return words
+  file_contents.each { |content| words << format(content.split.size) }
+  words
 end
 
-def bytes_count(filenames)
+def bytes_count(file_contents)
   bytes = []
-  filenames.each { |file| bytes << format(File.size(file)) }
-  return bytes
+  file_contents.each { |content| bytes << format(content.bytesize) }
+  bytes
 end
 
 def file_name(filenames)
-  file_name = []
-  filenames.each { |f| file_name << " #{f}" }
-  return file_name
+  filenames.map { |filename| " #{filename}" }
 end
 
 def output(row, total, filename)
   transposed = row.transpose
-  transposed.each { |row| puts row.join }
-  puts "#{total.join(' ')} total" if filename.size >= 2
-end
-
-def pipe_output(output)
-  print output.count("\n").to_s.rjust(8)
-  print output.split(/\s+/).count.to_s.rjust(8)
-  print output.bytesize.to_s.rjust(8)
+  transposed.each { |r| puts r.join }
+  puts "#{total.join} total" if filename.size >= 2
 end
 
 begin
-  if !ARGV.empty?
-  acquisition_filenames = ARGV
-  else
-  acquisition_filenames = [$stdin.read]
-# pipe_output($stdin.read)
-  end
-
   option = ARGV.getopts('lwc')
-  option['l'] = true && option['w'] = true && option['c'] = true if option['l'] == false && option['w'] == false && option['c'] == false && !ARGV.empty?
+  option['l'] = true && option['w'] = true && option['c'] = true if option['l'] == false && option['w'] == false && option['c'] == false
 
-  filename = file_name(acquisition_filenames)
-  file_content = file_read(acquisition_filenames)
+  if ARGV.any?
+    acquisition_filenames = ARGV
+    file_contents = file_read(acquisition_filenames)
+    filename = file_name(acquisition_filenames)
+  else
+    file_contents = [$stdin.read]
+    filename = []
+  end
 
   row = []
-  row << lines_count(acquisition_filenames) if option['l']
-  row << words_count(acquisition_filenames) if option['w']
-  row << bytes_count(acquisition_filenames) if option['c']
-  row << file_name(acquisition_filenames)
+  row << lines_count(file_contents) if option['l']
+  row << words_count(file_contents) if option['w']
+  row << bytes_count(file_contents) if option['c']
 
-  # binding.break
-  total = []
-  filename = filename.each { |f| f.strip! }
-  total << filename.sum { |file| File.read(file).count("\n") }.to_s.rjust(8) if option['l'] && filename.size >= 2
-  total << filename.sum { |file| File.read(file).split.size }.to_s.rjust(7) if option['w'] && filename.size >= 2
-  total << filename.sum { |file| File.size(file) }.to_s.rjust(7) if option['c'] && filename.size >= 2
-
-  unless ARGV.empty?
-    output(row, total, filename)
+  if ARGV.any?
+    row << file_name(acquisition_filenames)
+    filename = filename.each(&:strip!)
   end
+
+  total = []
+  total << format(filename.sum { |file| File.read(file).count("\n") }) if option['l'] && filename.size >= 2
+  total << format(filename.sum { |file| File.read(file).split.size }) if option['w'] && filename.size >= 2
+  total << format(filename.sum { |file| File.size(file) }) if option['c'] && filename.size >= 2
+
+  output(row, total, filename)
 rescue OptionParser::InvalidOption => e
   puts "Error Message: #{e.message}"
 end
