04d31c80d24f59580c4b689d9a19b18738d196a5
kurumadaisuke <kuruma0909@gmail.com>
2023-10-21 22:05:54 +0900

    機能実装完了。rubocopまだ

--- 07.ls_object/ls_command.rb
+++ 07.ls_object/ls_command.rb
@@ -13,8 +13,8 @@ class LsCommand
   end
 
   def output
-    @entries_list = @params_options.reverse_entries?(@entries_list)
-    default_format(@entries_list)
+    @entries_list = @params_options.reverse_entries_include_r?(@entries_list)
+    @params_options.format_entries_include_l? ? long_format(@entries_list) : default_format(@entries_list)
   end
 
   def default_format(entries_list)
@@ -42,5 +42,32 @@ class LsCommand
       max_length = name_length if name_length > max_length
     end
     max_length + 6
+  end
+
+  def long_format(entries_list)
+    total_block_size = total_block_size(entries_list)
+    puts "total #{total_block_size}"
+
+    entries_list.each do |entry|
+      row_text = [
+        entry.filetype,
+        entry.permission,
+        "#{entry.hard_link.rjust(3)} ",
+        "#{entry.user_name.ljust(14)} ",
+        "#{entry.group_name}  ",
+        "#{entry.byte_size.rjust(4)} ",
+        "#{entry.updated_date} ",
+        entry.name
+      ]
+      puts row_text.join
+    end
+  end
+
+  def total_block_size(entries_list)
+    total_block_size = 0
+    entries_list.each do |entry|
+      total_block_size += entry.block_size
+    end
+    total_block_size
   end
 end
